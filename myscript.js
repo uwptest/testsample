82f9dc7595ef6665212d098f856cdd76a5c37b39
uwptest <78918820+uwptest@users.noreply.github.com>
2021-02-11 23:03:47 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@
 function myfunction(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
-window.external.notify("data");
+window.external.notify("#" + randomColor);
 
 }
 
